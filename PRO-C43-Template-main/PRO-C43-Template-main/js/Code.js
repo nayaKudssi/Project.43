6b4fc295d08405b2db96266df3f4bb20776547cb
nayaKudssi <76819985+nayaKudssi@users.noreply.github.com>
2021-05-16 10:18:39 -0500

    Add files via upload

---
+++ PRO-C43-Template-main/PRO-C43-Template-main/js/Code.js
@@ -0,0 +1,24 @@
+const accessCode1 = "VARIABLE";
+const accessCode2 = "FUNCTION";
+const accessCode3 = "DATABASE";
+
+function clues() 
+{
+    fill("white");
+    textSize(15);
+    text("R E V B A I L A", 100,50);
+    fill("lightblue");
+    text("Hint: Always changing, not constant!!", 100,70);
+
+    fill("white");
+    textSize(15);
+    text("C U T N I F O N", 700,155);
+    fill("lightGreen");
+    text("Hint: Performs a particular task!!", 700,175);
+    
+    fill("white");
+    textSize(15);
+    text("A T A D A S B A", 285,295);
+    fill("Red");
+    text("Hint: Stores all information!!",285,320);
+}
